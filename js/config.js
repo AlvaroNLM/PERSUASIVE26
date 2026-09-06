155f26124cde3a2b49134477ba1440fe37658895
AlvaroNLM <alvaronavarrolopez@hotmail.com>
2026-09-06 18:30:50 +0200

    Testing...

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Public settings only. Paste the deployed Google Apps Script /exec URL here.
-export const APPS_SCRIPT_URL = '';
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxs0-jH3Px4DBmExXAzOXc1wj1G8zaNor-tQoBwJcgtmXfQpy91b5oMaoyitiuGg5-P/exec';
 export const TEST_MODE = true; //ture testing, false when publishing
 export const EXPERIMENT_VERSION = '2.0.0';
 export const CASE_VERSION = 'alex_v1';
